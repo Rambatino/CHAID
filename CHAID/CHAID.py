1480305516648d16aa9e02ec2abc0067d1601cb9
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2016-06-13 14:48:31 +0100

    Get CHAID working

--- CHAID/CHAID.py
+++ CHAID/CHAID.py
@@ -6,8 +6,10 @@ import collections as cl
 from treelib import Node, Tree
 import sys
 
+import ipdb
+
 DEFAULT_CONDITIONS =  {
-	'alpha_merge': 0.05,
+	'alpha_merge': .05,
 	'max_depth': 3,
 	'min_sample': 30
 }	
@@ -25,85 +27,79 @@ def df_to_tree(ind_df, dep_series, conditions):
 	ind_df = ind_df.fillna(-1.0)
 	ind_values = ind_df.values
 	dep_values = dep_series.values
-	tree(params, ind_values, dep_values, new_conditions)
-	return 1
+	return chaid_tree(params, ind_values, dep_values, new_conditions)
 
-def tree(params, ind, dep, conditions):
-	split = generate_best_split(ind, dep, conditions)
+def chaid_tree(params, ind, dep, conditions, depth=0):
+	depth = depth + 1
 
+	if conditions['max_depth'] < depth:
+		return
 
+	best_case = generate_best_split(ind, dep, conditions)
 
-	return 1
+	if best_case[0] is None:
+		return
 
+	tree = [best_case]
+
+	for choices in best_case[1]:
+		correct_rows = np.in1d(ind[:,best_case[0]], choices)
+		ind_slice = ind[correct_rows,:]
+		dep_slice = dep[correct_rows]
+
+		tree.append(chaid_tree(params, ind_slice, dep_slice, conditions, depth))
+
+	return tree
 
 def generate_best_split(ind, dep, conditions):
+	most_sig_ind = (None, None, None, 1)
 	for i in range(0, ind.shape[1]):
-		data = {}
-		index = ind[:,i]
-		unique = np.unique(index)
-		unique_list = list(it.combinations(unique, 2))
-		combs = np.array(list(it.combinations(unique, 2)))
-		import ipdb; ipdb.set_trace()
-		while True:
-			sub_data = np.ndarray(shape=(len(combs), 3), dtype=object, order='F')
-			for j, comb in enumerate(combs):
+		index = np.array(ind[:,i])
+		unique = set(index)
+
+		mappings = {}
+
+		
+		while len(unique) > 1:
+			size  = (len(unique) * (len(unique) - 1) )/ 2
+			sub_data = np.ndarray(shape=(size, 3), dtype=object, order='F')
+			for j, comb in enumerate(it.combinations(unique, 2)):
 				y = np.where(np.in1d(index, comb[0]))[0]
 				g = np.where(np.in1d(index, comb[1]))[0]
+
 				uni_y = dict(np.transpose(np.unique(dep[y], return_counts=True)))
 				uni_g = dict(np.transpose(np.unique(dep[g], return_counts=True)))
-				diff_1 = set(uni_y.keys()) - set(uni_g.keys())
-				uni_g.update(dict(map(lambda x: [x, 0], diff_1)))
-				diff_2 = set(uni_g.keys()) - set(uni_y.keys())
-				uni_y.update(dict(map(lambda x: [x, 0], diff_2)))
-				chi = stats.chi2_contingency(np.array([uni_y.values(), uni_g.values()]))
-				sub_data[j] = [comb, chi[0], chi[1]]
+
+				keys = set(uni_y.keys() + uni_g.keys())
+
+				cr_table = [
+					[ uni_y.get(k, 0) for k in keys],
+					[ uni_g.get(k, 0) for k in keys]
+				]
+
+				chi = stats.chi2_contingency(np.array(cr_table))
+				sub_data[j] = (comb, chi[0], chi[1])
+
 			highest_p_split = np.sort(sub_data[:,2])[-1]
 			correct_row = np.where(np.in1d(sub_data[:,2], highest_p_split))[0][0]
-			split = list(sub_data[correct_row, 0])
-			if len(combs) == 1 or highest_p_split < conditions['alpha_merge']:
-				data[str(i)] = sub_data
+
+			if size == 1 or highest_p_split < conditions['alpha_merge']:
+				if highest_p_split < most_sig_ind[3]:
+					most_sig_ind = (i, [ mappings.get(x,[x]) for x in unique ], sub_data[correct_row][1], highest_p_split)
 				break
-			combs = combs.astype(object)
-			drop = []
-			for tt, aa in enumerate(combs): 
-				substitute(aa, split)
-				ff = np.where(np.in1d(aa, split))
-				if len(ff[0]) == 1:
-					combs[tt, ff[0][0]] = split
-					if is_equal(combs[tt - 1], combs[tt]):
-						drop.append(tt)
-				elif len(ff[0]) == 2:
-					drop.append(tt)
-			combs = np.delete(combs, drop, 0)
 
+			split = list(sub_data[correct_row, 0])
 
-	return 5
+			if split[1] in mappings:
+				mappings[split[0]] =  mappings.get(split[0], [split[0]]) + mappings[split[1]]
+				del mappings[split[1]]
+			else:
+				mappings[split[0]] = mappings.get(split[0], [split[0]]) + [split[1]]
 
+			index[ index == split[1] ] = split[0]
+			unique.remove(split[1])
 
-def flatten_list(ndarr):
-	new_list = []
-	# for sublist in ndarr:
-	# 	if isinstance(sublist, list): 
-
-	return 'lol'
-
-
-def substitute(element_row, current_split):
-	import ipdb; ipdb.set_trace()
-	new_array = []
-	for sublist in current_split:
-		if isinstance(sublist, list):
-			for item in sublist:
-				new_array.append(item)
-	# flattened_array = [item for sublist in current_split (for item in sublist if isinstance(item, list) else item)]
-	for item in element_row:
-		if isinstance(item, np.ndarray): 
-			substitute(item, current_split)
-		elif np.in1d(flattened_array, [item]).sum() == 1:
-			element_row[np.where(np.in1d(flattened_array, [item]))[0][0]] = current_split
-	return 50
-
-
+	return most_sig_ind
 	
 
 # def find_index(arr_maj, arr_min):
@@ -138,4 +134,4 @@ if __name__ == "__main__":
 	df = pd.read_csv(sys.argv[1])
 	ind_df = df[['Umsatz', 'V5001', 'V5002_1', 'V5002_2', 'V5002_3', 'V5002_4', 'V6001', 'V6002']]
 	dep_series = df['titypv']
-	df_to_tree(ind_df, dep_series, {})
+	print df_to_tree(ind_df, dep_series, {})
