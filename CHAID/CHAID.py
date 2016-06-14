3e3528f440aa72a491ade5203f7863ca21df73b3
Mark Ramotowski <mark@intellectionsoftware.com>
2016-06-14 09:02:48 +0100

    Classified

--- CHAID/CHAID.py
+++ CHAID/CHAID.py
@@ -5,12 +5,6 @@ import numpy as np
 import collections as cl
 from treelib import Node, Tree
 
-DEFAULT_CONDITIONS =  {
-	'alpha_merge': .05,
-	'max_depth': 2,
-	'min_sample': 30
-}	
-
 class Node(object):
 	def __init__(self, choices=None, members={}, split_variable=None, chi=0, p=0, terminal_indices=[], id=0, parent=None):
 		self.choices = choices
@@ -35,117 +29,132 @@ class Node(object):
 		return str((self.choices, self.members, self.split_variable, self.chi, self.p))
 
 
-def conditions_merged(conditions):
-	new_conditions = DEFAULT_CONDITIONS.copy()
-	new_conditions.update(conditions)
-	return new_conditions
+class Split(object):
+	def __init__(self, index, splits, chi, p):
+		self.index = index
+		self.splits = splits
+		self.chi = chi
+		self.p = p
 
-def chaid(ind_df, dep_series, conditions):
-	new_conditions = conditions_merged(conditions)
-	params = {
-		'independent_variable_names': ind_df.columns
-	}
-	ind_df.apply(lambda x: x.fillna('-1.0', inplace=True) if (x.dtype == object) else x.fillna(-1.0, inplace=True))
-	ind_values = ind_df.values
-	dep_values = dep_series.values
-	tree, node_id =  chaid_node(params, np.arange(0, dep_values.shape[0] + 1, dtype=np.int), ind_values, dep_values, new_conditions)
-	return tree
 
-def chaid_node(params, rows, ind, dep, conditions, depth=0, tree_store=[], parent=None, node_id=0, parent_decisions=None):
-	depth = depth + 1
+class CHAID(object):
+	DEFAULT_CONDITIONS =  { 'alpha_merge': .05, 'max_depth': 2, 'min_sample': 30 }	
 
-	uni = dict(np.transpose(np.unique(dep, return_counts=True)))
+	def from_pandas_df(self, ind_df, dep_series, conditions):
+		new_conditions = self.DEFAULT_CONDITIONS.copy()
+		new_conditions.update(conditions)
+		params = {
+			'independent_variable_names': ind_df.columns
+		}
+		ind_df.apply(lambda x: x.fillna('-1.0', inplace=True) if (x.dtype == object) else x.fillna(-1.0, inplace=True))
+		ind_values = ind_df.values
+		dep_values = dep_series.values
+		self.data_size = dep_values.shape[0]
+		self.tree_data, node_id =  self.node(params, np.arange(0, dep_values.shape[0] + 1, dtype=np.int), ind_values, dep_values, new_conditions)
+		return self
 
-	if conditions['max_depth'] < depth:
-		terminal_node = Node(choices=parent_decisions, members=uni, id=node_id, parent=parent, terminal_indices=rows)
-		tree_store.append(terminal_node)
-		return tree_store, node_id + 1
+	def node(self, params, rows, ind, dep, conditions, depth=0, tree_store=[], parent=None, node_id=0, parent_decisions=None):
+		depth = depth + 1
 
-	split_variable, split_keys, chi, p = generate_best_split(ind, dep, conditions)
+		members = dict(np.transpose(np.unique(dep, return_counts=True)))
 
-	node = Node(choices=parent_decisions, members=uni, id=node_id, parent=parent, split_variable=split_variable, chi=chi, p=p)
-	tree_store.append(node)
-	parent = node_id
-	node_id = node_id + 1
+		if conditions['max_depth'] < depth:
+			terminal_node = Node(choices=parent_decisions, members=members, id=node_id, parent=parent, terminal_indices=rows)
+			tree_store.append(terminal_node)
+			return tree_store, node_id + 1
 
-	if split_variable is None:
+		split = self.generate_best_split(ind, dep, conditions)
+
+		node = Node(choices=parent_decisions, members=members, id=node_id, parent=parent, split_variable=split.index, chi=split.chi, p=split.p)
+		tree_store.append(node)
+		parent = node_id
+		node_id = node_id + 1
+
+		if split.index is None:
+			return tree_store, node_id
+
+		for choices in split.splits:
+			correct_rows = np.in1d(ind[:, split.index], choices)
+			dep_slice = dep[correct_rows]
+			ind_slice = ind[correct_rows, :]
+			row_slice = rows[correct_rows]
+			if conditions['min_sample'] < len(dep_slice):
+				tree_store, node_id = self.node(params, row_slice, ind_slice, dep_slice, conditions, depth, node_id=node_id, parent=parent, tree_store=tree_store, parent_decisions=choices)
+			else:
+				memebers = dict(np.transpose(np.unique(dep_slice, return_counts=True)))
+				terminal_node = Node(choices=choices, members=memebers, id=node_id, parent=parent, terminal_indices=row_slice)
+				tree_store.append(terminal_node)
+				node_id = node_id + 1
+
 		return tree_store, node_id
 
-	for choices in split_keys:
-		correct_rows = np.in1d(ind[:, split_variable], choices)
-		dep_slice = dep[correct_rows]
-		ind_slice = ind[correct_rows, :]
-		row_slice = rows[correct_rows]
-		if conditions['min_sample'] < len(dep_slice):
-			tree_store, node_id = chaid_node(params, row_slice, ind_slice, dep_slice, conditions, depth, node_id=node_id, parent=parent, tree_store=tree_store, parent_decisions=choices)
-		else:
-			uni = dict(np.transpose(np.unique(dep_slice, return_counts=True)))
-			terminal_node = Node(choices=choices, members=uni, id=node_id, parent=parent, terminal_indices=row_slice)
-			tree_store.append(terminal_node)
-			node_id = node_id + 1
+	def generate_best_split(self, ind, dep, conditions):
+		split = Split(None, None, None, 1)
+		for i in range(0, ind.shape[1]):
+			index = np.array(ind[:,i])
+			unique = set(index)
 
-	return tree_store, node_id
+			mappings = {}
 
-def generate_best_split(ind, dep, conditions):
-	most_sig_ind = (None, None, None, 1)
-	for i in range(0, ind.shape[1]):
-		index = np.array(ind[:,i])
-		unique = set(index)
+			while len(unique) > 1:
+				size  = (len(unique) * (len(unique) - 1)) / 2
+				sub_data = np.ndarray(shape=(size, 3), dtype=object, order='F')
+				for j, comb in enumerate(it.combinations(unique, 2)):
+					y = np.where(np.in1d(index, comb[0]))[0]
+					g = np.where(np.in1d(index, comb[1]))[0]
 
-		mappings = {}
+					uni_y = dict(np.transpose(np.unique(dep[y], return_counts=True)))
+					uni_g = dict(np.transpose(np.unique(dep[g], return_counts=True)))
 
-		while len(unique) > 1:
-			size  = (len(unique) * (len(unique) - 1)) / 2
-			sub_data = np.ndarray(shape=(size, 3), dtype=object, order='F')
-			for j, comb in enumerate(it.combinations(unique, 2)):
-				y = np.where(np.in1d(index, comb[0]))[0]
-				g = np.where(np.in1d(index, comb[1]))[0]
+					keys = set(uni_y.keys() + uni_g.keys())
 
-				uni_y = dict(np.transpose(np.unique(dep[y], return_counts=True)))
-				uni_g = dict(np.transpose(np.unique(dep[g], return_counts=True)))
+					cr_table = [
+						[ uni_y.get(k, 0) for k in keys ],
+						[ uni_g.get(k, 0) for k in keys ]
+					]
 
-				keys = set(uni_y.keys() + uni_g.keys())
+					chi = stats.chi2_contingency(np.array(cr_table))
+					sub_data[j] = (comb, chi[0], chi[1])
 
-				cr_table = [
-					[ uni_y.get(k, 0) for k in keys],
-					[ uni_g.get(k, 0) for k in keys]
-				]
+				highest_p_split = np.sort(sub_data[:, 2])[-1]
+				correct_row = np.where(np.in1d(sub_data[:, 2], highest_p_split))[0][0]
 
-				chi = stats.chi2_contingency(np.array(cr_table))
-				sub_data[j] = (comb, chi[0], chi[1])
+				if size == 1 or highest_p_split < conditions['alpha_merge']:
+					if highest_p_split < split.p:
+						split = Split(i, [ mappings.get(x, [x]) for x in unique ], sub_data[correct_row][1], highest_p_split)
+					break
 
-			highest_p_split = np.sort(sub_data[:, 2])[-1]
-			correct_row = np.where(np.in1d(sub_data[:, 2], highest_p_split))[0][0]
+				choice = list(sub_data[correct_row, 0])
 
-			if size == 1 or highest_p_split < conditions['alpha_merge']:
-				if highest_p_split < most_sig_ind[3]:
-					most_sig_ind = (i, [ mappings.get(x, [x]) for x in unique ], sub_data[correct_row][1], highest_p_split)
-				break
+				if choice[1] in mappings:
+					mappings[choice[0]] =  mappings.get(choice[0], [choice[0]]) + mappings[choice[1]]
+					del mappings[choice[1]]
+				else:
+					mappings[choice[0]] = mappings.get(choice[0], [choice[0]]) + [choice[1]]
 
-			split = list(sub_data[correct_row, 0])
+				index[ index == choice[1] ] = choice[0]
+				unique.remove(choice[1])
 
-			if split[1] in mappings:
-				mappings[split[0]] =  mappings.get(split[0], [split[0]]) + mappings[split[1]]
-				del mappings[split[1]]
-			else:
-				mappings[split[0]] = mappings.get(split[0], [split[0]]) + [split[1]]
+		return split
 
-			index[ index == split[1] ] = split[0]
-			unique.remove(split[1])
+	def to_tree(self):
+		tree = Tree()
+		for node in self.tree_data:
+			tree.create_node(node, node.id, parent=node.parent)
+		return tree
 
-	return most_sig_ind
+	def print_tree(self):
+		self.to_tree().show()
 
-def to_tree(tree_data):
-	tree = Tree()
-	for node in tree_data:
-		tree.create_node(node, node.id, parent=node.parent)
-	return tree
+	def predict(self):
+		pred = np.zeros(self.data_size)
+		for node in self.tree_data:
+			pred[node.terminal_indices] = node.id
+		return pred
 
-def model_prediction(tree_data, length):
-	pred = np.zeros(length)
-	for node in tree_data:
-		pred[node.terminal_indices] = node.id
-	return pred
+	def __repr__(self):
+		 return str(self.tree_data)
+
 
 if __name__ == "__main__":
 	import argparse
@@ -170,4 +179,4 @@ if __name__ == "__main__":
 		config['alpha_merge'] = nspace.alpha_merge
 	if nspace.min_samples:
 		config['min_sample'] = nspace.min_samples
-	to_tree(chaid(ind_df, dep_series, config)).show()
+	print CHAID().from_pandas_df(ind_df, dep_series, config)
