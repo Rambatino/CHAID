3a967ad8bc83420f2d41edfeda921232c7cd287c
Mark Ramotowski <mark@intellectionsoftware.com>
2016-06-15 12:28:52 +0100

    V 1.1

--- setup.py
+++ setup.py
@@ -10,11 +10,14 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='CHAID',
-    version='1.0',
+    version='1.1',
     description='A CHAID tree building algorithm',
-    long_description='CHAID enables the building of decision trees using the Chi-Squared test for significance. As it is built on numpy dataframes, it works well with pandas.',
+    long_description=long_description,
     url='https://github.com/Rambatino/CHAID',
     author='Mark Ramotowski',
     author_email='mark.tint.ramotowski@gmail.com',
@@ -38,6 +41,5 @@ setup(
     extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
-    },
-    data_files=[('my_data', ['example_files/CHAID.csv'])],
+    }
 )
