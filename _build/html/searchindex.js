4608cf35114a002f1866c8b4db3de6445fb36194
Mark Ramotowski <mark@intellectionsoftware.com>
2016-06-18 02:11:07 +0100

    Added sphinx documentation

---
+++ _build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["README","index"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,modul:1,page:1,search:1},titles:["&lt;no title&gt;","Welcome to CHAID&#8217;s documentation!"],titleterms:{chaid:1,document:1,indic:1,tabl:1,welcom:1}})
