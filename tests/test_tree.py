2a5319eea1b406d294317e16a02be4c0266b0aa2
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2016-06-24 17:19:25 +0100

    Test cases for lazy loading

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,9 +1,11 @@
 """
 Testing module for the class CHAIDTree
 """
-import numpy as np
 import collections
+from unittest import TestCase
+import numpy as np
 import CHAID
+
 
 def list_unordered_equal(a, b):
     """ Compares the unordered contents of two nd lists"""
@@ -14,12 +16,14 @@ def list_unordered_equal(a, b):
     else:
         return a == b
 
+
 def list_ordered_equal(a, b):
     """ Compares the unordered contents of two nd lists"""
     if isinstance(a, collections.Iterable) and isinstance(b, collections.Iterable):
         return all(list_ordered_equal(i_a, i_b) for i_a, i_b in zip(a, b))
     else:
         return a == b
+
 
 def test_best_split():
     """
@@ -42,3 +46,27 @@ def test_best_split():
     assert list_unordered_equal(split.surrogates, []), 'No surrogates should be generated'
     assert split.p < 0.015
 
+
+class TestTreeGenerated(TestCase):
+    """ Test case class to check that the tree is correcly lazy loaded """
+    def setUp(self):
+        arr = np.array(([1] * 5) + ([2] * 5))
+        orig_arr = np.array(([1] * 5) + ([2] * 5))
+        ndarr = np.array(([1, 2, 3] * 5) + ([2, 2, 3] * 5)).reshape(10, 3)
+        orig_ndarr = np.array(([1, 2, 3] * 5) + ([2, 2, 3] * 5)).reshape(10, 3)
+        self.tree = CHAID.CHAID(ndarr, arr)
+
+    def test_iter(self):
+        """ Test the calls to __iter__() populate the tree """
+        self.tree.__iter__()
+        assert(self.tree.tree_store is not None)
+
+    def test_modification(self):
+        """ Test the calls to get_node() populate the tree """
+        self.tree.get_node(0)
+        assert(self.tree.tree_store is not None)
+
+    def test_deletion(self):
+        """ Test the calls to build_tree() populate the tree """
+        self.tree.build_tree()
+        assert(self.tree.tree_store is not None)
