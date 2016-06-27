4282b9ab9039109d6cc8dc37cc4abed09990956a
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2016-06-27 16:09:00 +0100

    Add docstrings to testing

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -41,17 +41,21 @@ class TestDeepCopy(TestCase):
     """ Test fixture class for deep copy method """
     def setUp(self):
         """ Setup for copy tests"""
-        arr = np.array([5.0, 10.0])
+        # Use string so numpy array dtype is object and may store references
+        arr = np.array(['5.0', '10.0'])
         self.orig = CHAID.CHAIDVector(arr)
         self.copy = self.orig.deep_copy()
 
     def test_deep_copy_does_copy(self):
+        """ Ensure a copy actually happens when deep_copy is called"""
         assert id(self.orig) != id(self.copy), 'The vector objects must be different'
         assert list_ordered_equal(self.copy, self.orig), 'Vector contents must be the same'
 
     def test_alteration_doesnt_affect_original(self):
+        """ Test that altering the copy doesn't alter the original """
         self.copy.arr[0] = 55.0
         assert not list_ordered_equal(self.copy, self.orig), 'Altering one vector should not affected the other'
 
     def test_metadata(self):
+        """ Ensure metadata is copied correctly or deep_copy """
         assert self.copy.metadata == self.orig.metadata, 'Copied metadata should be equivilent'
