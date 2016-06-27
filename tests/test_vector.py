30c8306563019f97deb8511c201a435de578974b
Mark Ramotowski <mark@intellectionsoftware.com>
2016-06-27 11:33:15 +0100

    Reformatted spec

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -7,35 +7,34 @@ import numpy as np
 import CHAID
 
 
-class TestVectorGenerated(TestCase):
-    def test_chaid_vector_converts_strings_with_correct_metadata(self):
-        """ Checking that the metadata is correct when CHAIDVectors are created from strings """
-        arr = np.array(['1', '2'])
-        vector = CHAID.CHAIDVector(arr)
+def test_chaid_vector_converts_strings_with_correct_metadata():
+    """ Checking that the metadata is correct when CHAIDVectors are created from strings """
+    arr = np.array(['1', '2'])
+    vector = CHAID.CHAIDVector(arr)
 
-        assert np.array_equal(vector.arr, np.array([0.0, 1.0])), 'The indices are correctly substituted'
-        assert vector._metadata == {0.0: '1', 1.0: '2', -1.0: '<missing>'}, 'The metadata is formed correctly'
+    assert np.array_equal(vector.arr, np.array([0.0, 1.0])), 'The indices are correctly substituted'
+    assert vector._metadata == {0.0: '1', 1.0: '2', -1.0: '<missing>'}, 'The metadata is formed correctly'
 
-    def test_chaid_vector_does_not_convert_ints(self):
-        """ Checking that the metadata is correct when CHAIDVectors are created from ints """
-        arr = np.array([1, 2])
-        vector = CHAID.CHAIDVector(arr)
+def test_chaid_vector_does_not_convert_ints():
+    """ Checking that the metadata is correct when CHAIDVectors are created from ints """
+    arr = np.array([1, 2])
+    vector = CHAID.CHAIDVector(arr)
 
-        assert np.array_equal(vector.arr, np.array([1, 2])), 'The indices are correctly substituted'
-        assert vector._metadata == {-1.0: '<missing>'}, 'The metadata is formed correctly'
+    assert np.array_equal(vector.arr, np.array([1, 2])), 'The indices are correctly substituted'
+    assert vector._metadata == {-1.0: '<missing>'}, 'The metadata is formed correctly'
 
-    def test_chaid_vector_does_not_convert_floats(self):
-        """ Checking that the metadata is correct when CHAIDVectors are created from floats """
-        arr = np.array([1.0, 2.0])
-        vector = CHAID.CHAIDVector(arr)
+def test_chaid_vector_does_not_convert_floats():
+    """ Checking that the metadata is correct when CHAIDVectors are created from floats """
+    arr = np.array([1.0, 2.0])
+    vector = CHAID.CHAIDVector(arr)
 
-        assert np.array_equal(vector.arr, np.array([1.0, 2.0])), 'The indices are correctly substituted'
-        assert vector._metadata == {-1.0: '<missing>'}, 'The metadata is formed correctly'
+    assert np.array_equal(vector.arr, np.array([1.0, 2.0])), 'The indices are correctly substituted'
+    assert vector._metadata == {-1.0: '<missing>'}, 'The metadata is formed correctly'
 
-    def test_chaid_vector_converts_ints_when_dtype_is_object(self):
-        """ Checking that the metadata is correct when CHAIDVectors are created from ints """
-        arr = np.array([1, 2], dtype="object")
-        vector = CHAID.CHAIDVector(arr)
+def test_chaid_vector_converts_ints_when_dtype_is_object():
+    """ Checking that the metadata is correct when CHAIDVectors are created from ints """
+    arr = np.array([1, 2], dtype="object")
+    vector = CHAID.CHAIDVector(arr)
 
-        assert np.array_equal(vector.arr, np.array([0.0, 1.0])), 'The indices are correctly substituted'
-        assert vector._metadata == {0.0: 1, 1.0: 2, -1.0: '<missing>'}, 'The metadata is formed correctly'
+    assert np.array_equal(vector.arr, np.array([0.0, 1.0])), 'The indices are correctly substituted'
+    assert vector._metadata == {0.0: 1, 1.0: 2, -1.0: '<missing>'}, 'The metadata is formed correctly'
