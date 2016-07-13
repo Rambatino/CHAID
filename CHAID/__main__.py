12d3a92cbe6dec49e66d13bca25d1a9aae037276
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2016-07-13 15:57:03 +0100

    Update command line arguments to track master branch

--- CHAID/__main__.py
+++ CHAID/__main__.py
@@ -1,23 +1,32 @@
 import argparse
 import pandas as pd
-from CHAID import CHAID
+from .CHAID import CHAID
 
 
 def main():
     """Entry point when module is run from command line"""
 
-    parser = argparse.ArgumentParser(description='Run the chaid algorithm on a csv file.')
+    parser = argparse.ArgumentParser(description='Run the chaid algorithm on a'
+                                     ' csv file.')
     parser.add_argument('file')
     parser.add_argument('dependent_variable', nargs=1)
     parser.add_argument('independent_variables', nargs='+')
 
-    parser.add_argument('--max-depth', type=int, help='Max depth of generated tree')
-    parser.add_argument('--min-samples', type=int, help='Minimum number of samples required to split node')
+    parser.add_argument('--max-depth', type=int, help='Max depth of generated '
+                        'tree')
+    parser.add_argument('--min-samples', type=int, help='Minimum number of '
+                        'samples required to split node')
     parser.add_argument('--alpha-merge', type=float, help='Alpha Merge')
-    parser.add_argument('--classify', action='store_true', help='Classify input rather than printing the tree')
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('--classify', action='store_true', help='Add column to'
+                       ' input with the node id of the node that that '
+                       'respondent has been placed into')
+    group.add_argument('--predict', action='store_true', help='Add column to '
+                       'input with the value of the  dependent varaible that '
+                       'the majority of respondents in that node selected')
     nspace = parser.parse_args()
 
-    df = pd.read_csv(nspace.file)
+    data = pd.read_csv(nspace.file)
 
     config = {}
     if nspace.max_depth:
@@ -26,16 +35,21 @@ def main():
         config['alpha_merge'] = nspace.alpha_merge
     if nspace.min_samples:
         config['min_sample'] = nspace.min_samples
-    tree = CHAID.from_pandas_df(df, nspace.independent_variables, nspace.dependent_variable[0], **config)
+    tree = CHAID.from_pandas_df(data, nspace.independent_variables,
+                                nspace.dependent_variable[0], **config)
 
     if nspace.classify:
-        predictions = pd.Series(tree.predict())
+        predictions = pd.Series(tree.node_predictions())
         predictions.name = 'node_id'
-        df = pd.concat([df, predictions], axis=1)
-        print(df.to_csv())
+        data = pd.concat([data, predictions], axis=1)
+        print(data.to_csv())
+    elif nspace.predictions:
+        predictions = pd.Series(tree.model_predictions())
+        predictions.name = 'predicted'
+        data = pd.concat([data, predictions], axis=1)
+        print(data.to_csv())
     else:
         tree.print_tree()
-
 
 
 if __name__ == "__main__":
