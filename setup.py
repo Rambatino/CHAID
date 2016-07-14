66e23d1cb4bc9fb17e1816b6f7ef51559b3a8f64
Mark Ramotowski <mark@intellectionsoftware.com>
2016-07-14 14:29:32 +0100

    Bumped to v0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from os import path
 
 setup(
     name='CHAID',
-    version='0.2.0',
+    version='0.3.0',
     description='A CHAID tree building algorithm',
     long_description="This package provides a python implementation of the Chi-Squared Automatic Inference Detection (CHAID) decision tree",
     url='https://github.com/Rambatino/CHAID',
