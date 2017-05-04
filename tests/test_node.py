309e6e398ba1a22f0e8b8e8988109f4f09224022
Mark Ramotowski <mark@intellectionsoftware.com>
2017-05-04 15:28:02 +0200

    Switched chi to score in node

--- tests/test_node.py
+++ tests/test_node.py
@@ -14,3 +14,12 @@ def test_dependent_variable_properties_as_members_for_continous_node():
     node = CHAID.Node(dep_v=continuous_dp)
     assert node.members == {'mean': data.mean(), 's.t.d': data.std()}
 
+def test_node_should_have_a_score():
+    """
+    Tests that node uses the split score and is called 'score'
+    """
+    data = np.array([1.3, 23.0, 3.0, 3.0, 12.4])
+    continuous_dp = CHAID.ContinuousColumn(data)
+    split = CHAID.Split("a", [], 2, 3, 4)
+    node = CHAID.Node(dep_v=continuous_dp, split=split)
+    assert node.score == 2
