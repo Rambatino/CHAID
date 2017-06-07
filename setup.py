05696c8fb3f55876d4a617199397b6852bd2f500
Mark Ramotowski <mark@intellectionsoftware.com>
2017-06-07 11:54:46 +0100

    Changed licence status

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
