b1f75700437d825654aaf508367209e4a752d82e
Martijn Pieters <github.com@zopatista.com>
2020-01-07 14:35:41 +0000

    Refactor Graph to avoid cross-platform issues
    This uses
    
    - `tempfile.TemporaryDirectory()` to handle cleaning up temporary files for the images. This isn't available in Python 2.7 but a simple backport is included (the standard-library version goes a little further in cleaning up still, should not be needed here).
    - `tempfile.mkstemp()` to generate image filenames, those are guaranteed to be unique without needing to generate filenames based on the current timestamp.
    - I cleaned up the code to be Flake-8 clean and better factored.

--- CHAID/graph.py
+++ CHAID/graph.py
@@ -1,10 +1,26 @@
-from graphviz import Digraph
-import datetime
-import time
+import os
+from datetime import datetime
+
 import plotly.graph_objs as go
 import plotly.io as pio
 import colorlover as cl
-import os
+from graphviz import Digraph
+
+try:
+    # Python 3.2 and newer
+    from tempfile import mkstemp, TemporaryDirectory
+except ImportError:
+    # minimal backport of TemporaryDirectory for Python 2.7, sufficient
+    # for use with this module.
+    import shutil
+    from tempfile import mkdtemp, mkstemp
+    class TemporaryDirectory(object):
+        def __init__(self):
+            self.name = mkdtemp()
+        def __enter__(self):
+            return self.name
+        def __exit__(self, *args):
+            shutil.rmtree(self.name, ignore_errors=True)
 
 class Graph(object):
     """
@@ -17,77 +33,71 @@ class Graph(object):
 
     def __init__(self, tree):
         self.tree = tree
-        self.files = []
 
     def render(self, path, view):
-        g = Digraph(format='png')
-        for node in self.tree:
-            m, p, score = node.members, None, None
-            if node.p is not None and node.score is not None:
-                p = str(round(node.p, 5))
-                score = str(round(node.score, 2))
-            g.node(str(node.node_id), '', {'shape': 'plaintext', 'image': self.bar_chart(
-                m, node.is_terminal, node.node_id == 0, p, score, node.split.column), 'labelloc': 'b'})
-            if node.parent is not None:
-                edge_label = '   ( ' + ', '.join(node.choices) + ')'
-                g.edge(str(node.parent), str(node.node_id), label=edge_label)
-        g.graph_attr['splines'] = 'ortho'
-        g.render(
-            'trees/' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '.gv'
-            if path is None
-            else path,
-            view=view
-        )
-        self.remove_tmp_files()
+        if path is None:
+            path = os.path.join("trees", "{:%Y-%m-%d %H:%M:%S}.gv".format(datetime.now()))
+        with TemporaryDirectory() as self.tempdir:
+            g = Digraph(
+                format="png",
+                graph_attr={"splines": "ortho"},
+                node_attr={"shape": "plaintext", "labelloc": "b"},
+            )
+            for node in self.tree:
+                image = self.bar_chart(node)
+                g.node(str(node.node_id), attributes={"image": image})
+                if node.parent is not None:
+                    edge_label = "   ( {})".format(', '.join(node.choices))
+                    g.edge(str(node.parent), str(node.node_id), label=edge_label)
+            g.render(path, view=view)
 
-    def bar_chart(self, members, is_terminal, is_root, p, score, split):
+    def bar_chart(self, node):
         fig = {
             "data": [
-              {
-                  "values": [v for v in members.values()],
-                  "labels": [k for k in members.keys()],
-                  "domain": {"x": [0, 1], "y":[0.4, 1.0]},
-                  "hole": .4,
-                  "type": "pie",
-                  'showlegend': True if is_root else False,
-                  'marker': {'colors': cl.scales['5']['qual']['Set1']}
-              }],
+                {
+                    "values": list(node.members.values()),
+                    "labels": list(node.members),
+                    "domain": {"x": [0, 1], "y": [0.4, 1.0]},
+                    "hole": 0.4,
+                    "type": "pie",
+                    "showlegend": node.node_id == 0,
+                    "marker": {"colors": cl.scales["5"]["qual"]["Set1"]},
+                }
+            ],
             "layout": {
-                "margin": dict(t=50),
+                "margin": {"t": 50},
                 "annotations": [
                     {
-                        "y": {'domain': [0, 0.2]},
-                        "font": {
-                            "size": 18
-                        },
+                        "font": {"size": 18},
                         "showarrow": False,
-                        "text": '',
+                        "text": "",
                         "x": 0.5,
-                        "y": 0.5
+                        "y": 0.5,
                     },
-                    {
-                        "y": {'domain': [0, 0.2]},
-                    }
-                ]
-            }
+                    {"y": {"domain": [0, 0.2]}},
+                ],
+            },
         }
-        if is_terminal is False: self.append_table(fig, [p, score, split])
-        file = '/tmp/' + ("%.20f" % time.time()).replace('.', '') + '.png'
-        pio.write_image(fig, file=file, format='png')
-        self.files.append(file)
-        return file
 
-    def append_table(self, fig, table_scores):
-        table = go.Table(
-            domain=dict(x=[0.3, 0.7], y=[0, 0.37]),
-            header=dict(height=0, line = dict(color = 'white'),
-                        fill = dict(color = 'white')),
-            cells=dict(values=[['<i>p</i>', 'score', 'splitting on'], table_scores],
-                       line=dict(color='#FFF'), align=['left'] * 5,
-                       font=dict(color=['rgb(40, 40, 40)'] * 5, size=12), height=27,
-                       fill=dict(color=['rgb(235, 193, 238)', 'rgba(228, 222, 249, 0.65)']))
+        if not node.is_terminal:
+            fig["data"].append(self._table(node))
+
+        filename = mkstemp(suffix=".png", dir=self.tempdir)[1]
+        pio.write_image(fig, file=filename, format="png")
+        return filename
+
+    def _table(self, node):
+        p = None if node.p is None else format(node.p, ".5f")
+        score = None if node.score is None else format(node.score, ".2f")
+        return go.Table(
+            domain={"x": [0.3, 0.7], "y": [0, 0.37]},
+            header={"height": 0, "line": {"color": "white"}, "fill": {"color": "white"}},
+            cells={
+                "values": [["<i>p</i>", "score", "splitting on"], [p, score, node.split.column],
+                "line": {"color": "#FFF"},
+                "align": ["left"] * 5,
+                "font": {"color": ["rgb(40, 40, 40)"] * 5, "size": 12,},
+                "height": 27,
+                "fill": {"color": ["rgb(235, 193, 238)", "rgba(228, 222, 249, 0.65)"]},
+            },
         )
-        fig['data'].append(table)
-
-    def remove_tmp_files(self):
-        [os.remove(file) for file in self.files]
